8e5bdc15188329663294d16ceb88bf56790f799a
trh4ckn0n <trhacknon@proton.me>
2025-04-11 04:43:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,30 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='trkn_dalletxt',
+    name='trkn-dalletxt',
     version='0.1.0',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
-        "openai",
-        "python-dotenv",
-        "requests",
-        "Pillow",
-        "questionary",
+        'openai',
+        'python-dotenv',
+        'requests',
+        'Pillow',
+        'questionary',
     ],
     entry_points={
         'console_scripts': [
-            'trkn-menu=menu:main',  # si `main()` est ton point d’entrée
+            'trkn-menu=trkn_dalletxt.menu:main',  # main() doit exister dans menu.py
         ],
     },
+    author='trhacknon',
+    description='Générateur d’images DALL·E avec prompt texte et customisation',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/trh4ckn0n/trkn-dalletxt',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
 )
